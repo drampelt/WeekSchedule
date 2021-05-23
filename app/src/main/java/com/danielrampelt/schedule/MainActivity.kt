73b1a7eae17b54949a12bc9caaa575dbe61600c9
Daniel Rampelt <drampelt@gmail.com>
2021-05-22 22:01:01 -0400

    feat: basic daily event schedule

--- app/src/main/java/com/danielrampelt/schedule/MainActivity.kt
+++ app/src/main/java/com/danielrampelt/schedule/MainActivity.kt
@@ -3,12 +3,36 @@ package com.danielrampelt.schedule
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.ParentDataModifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
 import com.danielrampelt.schedule.ui.theme.WeekScheduleTheme
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,22 +41,143 @@ class MainActivity : ComponentActivity() {
             WeekScheduleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Schedule(events = sampleEvents)
                 }
             }
         }
     }
 }
 
+data class Event(
+    val name: String,
+    val color: Color,
+    val start: LocalDateTime,
+    val end: LocalDateTime,
+    val description: String? = null,
+)
+
+val EventTimeFormatter = DateTimeFormatter.ofPattern("h:mm a")
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BasicEvent(
+    event: Event,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(end = 2.dp, bottom = 2.dp)
+            .background(event.color, shape = RoundedCornerShape(4.dp))
+            .padding(4.dp)
+    ) {
+        Text(
+            text = "${event.start.format(EventTimeFormatter)} - ${event.end.format(EventTimeFormatter)}",
+            style = MaterialTheme.typography.caption,
+        )
+
+        Text(
+            text = event.name,
+            style = MaterialTheme.typography.body1,
+            fontWeight = FontWeight.Bold,
+        )
+
+        if (event.description != null) {
+            Text(
+                text = event.description,
+                style = MaterialTheme.typography.body2,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
+    }
+}
+
+private val sampleEvents = listOf(
+    Event(
+        name = "Google I/O Keynote",
+        color = Color(0xFFAFBBF2),
+        start = LocalDateTime.parse("2021-05-18T09:00:00"),
+        end = LocalDateTime.parse("2021-05-18T11:00:00"),
+        description = "Tune in to find out about how we're furthering our mission to organize the world’s information and make it universally accessible and useful.",
+    ),
+    Event(
+        name = "Developer Keynote",
+        color = Color(0xFFAFBBF2),
+        start = LocalDateTime.parse("2021-05-18T11:15:00"),
+        end = LocalDateTime.parse("2021-05-18T12:15:00"),
+        description = "Learn about the latest updates to our developer products and platforms from Google Developers.",
+    ),
+    Event(
+        name = "What's new in Android",
+        color = Color(0xFF1B998B),
+        start = LocalDateTime.parse("2021-05-18T12:30:00"),
+        end = LocalDateTime.parse("2021-05-18T15:00:00"),
+        description = "In this Keynote, Chet Haase, Dan Sandler, and Romain Guy discuss the latest Android features and enhancements for developers.",
+    ),
+)
+
+class EventsProvider : PreviewParameterProvider<Event> {
+    override val values = sampleEvents.asSequence()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun EventPreview(
+    @PreviewParameter(EventsProvider::class) event: Event,
+) {
     WeekScheduleTheme {
-        Greeting("Android")
+        BasicEvent(event, modifier = Modifier.sizeIn(maxHeight = 64.dp))
+    }
+}
+
+private class EventDataModifier(
+    val event: Event,
+) : ParentDataModifier {
+    override fun Density.modifyParentData(parentData: Any?) = event
+}
+
+private fun Modifier.eventData(event: Event) = this.then(EventDataModifier(event))
+
+@Composable
+fun Schedule(
+    events: List<Event>,
+    modifier: Modifier = Modifier,
+    eventContent: @Composable (event: Event) -> Unit = { BasicEvent(event = it) },
+) {
+    val hourHeight = 64.dp
+    Layout(
+        content = {
+              events.sortedBy(Event::start).forEach { event ->
+                  Box(modifier = Modifier.eventData(event)) {
+                      eventContent(event)
+                  }
+              }
+        },
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) { measureables, constraints ->
+        val height = hourHeight.roundToPx() * 24
+        val placeablesWithEvents = measureables.map { measurable ->
+            val event = measurable.parentData as Event
+            val eventDurationMinutes = ChronoUnit.MINUTES.between(event.start, event.end)
+            val eventHeight = ((eventDurationMinutes / 60f) * hourHeight.toPx()).roundToInt()
+            val placeable = measurable.measure(constraints.copy(minHeight = eventHeight, maxHeight = eventHeight))
+            Pair(placeable, event)
+        }
+        layout(constraints.maxWidth, height) {
+            placeablesWithEvents.forEach { (placeable, event) ->
+                val eventOffsetMinutes = ChronoUnit.MINUTES.between(LocalTime.MIN, event.start.toLocalTime())
+                val eventY = ((eventOffsetMinutes / 60f) * hourHeight.toPx()).roundToInt()
+                placeable.place(0, eventY)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SchedulePreview() {
+    WeekScheduleTheme {
+        Schedule(sampleEvents)
     }
 }
