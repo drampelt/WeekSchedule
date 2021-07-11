bec83ee8f9c3d33ec01c0717a85a4a61b3690ef0
Daniel Rampelt <drampelt@gmail.com>
2021-07-10 21:35:10 -0400

    feat: basic concurrent events

--- app/src/main/java/com/danielrampelt/schedule/MainActivity.kt
+++ app/src/main/java/com/danielrampelt/schedule/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -88,6 +89,9 @@ data class PositionedEvent(
     val date: LocalDate,
     val start: LocalTime,
     val end: LocalTime,
+    val col: Int = 0,
+    val colSpan: Int = 1,
+    val colTotal: Int = 1,
 )
 
 val EventTimeFormatter = DateTimeFormatter.ofPattern("h:mm a")
@@ -104,6 +108,7 @@ fun BasicEvent(
         modifier = modifier
             .fillMaxSize()
             .padding(end = 2.dp, bottom = if (positionedEvent.splitType == SplitType.End) 0.dp else 2.dp)
+            .clipToBounds()
             .background(
                 event.color,
                 shape = RoundedCornerShape(
@@ -118,12 +123,16 @@ fun BasicEvent(
         Text(
             text = "${event.start.format(EventTimeFormatter)} - ${event.end.format(EventTimeFormatter)}",
             style = MaterialTheme.typography.caption,
+            maxLines = 1,
+            overflow = TextOverflow.Clip,
         )
 
         Text(
             text = event.name,
             style = MaterialTheme.typography.body1,
             fontWeight = FontWeight.Bold,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
         )
 
         if (event.description != null) {
@@ -148,30 +157,37 @@ private val sampleEvents = listOf(
     Event(
         name = "Developer Keynote",
         color = Color(0xFFAFBBF2),
-        start = LocalDateTime.parse("2021-05-18T11:15:00"),
-        end = LocalDateTime.parse("2021-05-18T12:15:00"),
+        start = LocalDateTime.parse("2021-05-18T09:00:00"),
+        end = LocalDateTime.parse("2021-05-18T10:00:00"),
         description = "Learn about the latest updates to our developer products and platforms from Google Developers.",
     ),
     Event(
         name = "What's new in Android",
         color = Color(0xFF1B998B),
-        start = LocalDateTime.parse("2021-05-18T12:30:00"),
-        end = LocalDateTime.parse("2021-05-18T15:00:00"),
+        start = LocalDateTime.parse("2021-05-18T10:00:00"),
+        end = LocalDateTime.parse("2021-05-18T11:00:00"),
         description = "In this Keynote, Chet Haase, Dan Sandler, and Romain Guy discuss the latest Android features and enhancements for developers.",
-    ),
-    Event(
-        name = "What's new in Machine Learning",
-        color = Color(0xFFF4BFDB),
-        start = LocalDateTime.parse("2021-05-19T09:30:00"),
-        end = LocalDateTime.parse("2021-05-19T11:00:00"),
-        description = "Learn about the latest and greatest in ML from Google. We’ll cover what’s available to developers when it comes to creating, understanding, and deploying models for a variety of different applications.",
     ),
     Event(
         name = "What's new in Material Design",
         color = Color(0xFF6DD3CE),
-        start = LocalDateTime.parse("2021-05-19T11:00:00"),
-        end = LocalDateTime.parse("2021-05-20T11:45:00"),
+        start = LocalDateTime.parse("2021-05-18T11:00:00"),
+        end = LocalDateTime.parse("2021-05-18T11:45:00"),
         description = "Learn about the latest design improvements to help you build personal dynamic experiences with Material Design.",
+    ),
+    Event(
+        name = "What's new in Machine Learning",
+        color = Color(0xFFF4BFDB),
+        start = LocalDateTime.parse("2021-05-18T10:00:00"),
+        end = LocalDateTime.parse("2021-05-18T11:00:00"),
+        description = "Learn about the latest and greatest in ML from Google. We’ll cover what’s available to developers when it comes to creating, understanding, and deploying models for a variety of different applications.",
+    ),
+    Event(
+        name = "What's new in Machine Learning",
+        color = Color(0xFFF4BFDB),
+        start = LocalDateTime.parse("2021-05-18T10:30:00"),
+        end = LocalDateTime.parse("2021-05-18T11:30:00"),
+        description = "Learn about the latest and greatest in ML from Google. We’ll cover what’s available to developers when it comes to creating, understanding, and deploying models for a variety of different applications.",
     ),
     Event(
         name = "Jetpack Compose Basics",
@@ -334,6 +350,68 @@ private fun splitEvents(events: List<Event>): List<PositionedEvent> {
         .flatten()
 }
 
+private fun PositionedEvent.overlapsWith(other: PositionedEvent): Boolean {
+    return date == other.date && start < other.end && end > other.start
+}
+
+private fun List<PositionedEvent>.timesOverlapWith(event: PositionedEvent): Boolean {
+    return any { it.overlapsWith(event) }
+}
+
+private fun arrangeEvents(events: List<PositionedEvent>): List<PositionedEvent> {
+    val positionedEvents = mutableListOf<PositionedEvent>()
+    val groupEvents: MutableList<MutableList<PositionedEvent>> = mutableListOf()
+
+    fun resetGroup() {
+        groupEvents.forEachIndexed { colIndex, col ->
+            col.forEach { e ->
+                positionedEvents.add(e.copy(col = colIndex, colTotal = groupEvents.size))
+            }
+        }
+        groupEvents.clear()
+    }
+
+    events.forEach { event ->
+        var firstFreeCol = -1
+        var numFreeCol = 0
+        for (i in 0 until groupEvents.size) {
+            val col = groupEvents[i]
+            if (col.timesOverlapWith(event)) {
+                if (firstFreeCol < 0) continue else break
+            }
+            if (firstFreeCol < 0) firstFreeCol = i
+            numFreeCol++
+        }
+
+        when {
+            // Overlaps with all, add a new column
+            firstFreeCol < 0 -> {
+                groupEvents += mutableListOf(event)
+                // Expand anything that spans into the previous column and doesn't overlap with this event
+                for (ci in 0 until groupEvents.size - 1) {
+                    val col = groupEvents[ci]
+                    col.forEachIndexed { ei, e ->
+                        if (ci + e.colSpan == groupEvents.size - 1 && !e.overlapsWith(event)) {
+                            col[ei] = e.copy(colSpan = e.colSpan + 1)
+                        }
+                    }
+                }
+            }
+            // No overlap with any, start a new group
+            numFreeCol == groupEvents.size -> {
+                resetGroup()
+                groupEvents += mutableListOf(event)
+            }
+            // At least one column free, add to first free column and expand to as many as possible
+            else -> {
+                groupEvents[firstFreeCol] += event.copy(colSpan = numFreeCol)
+            }
+        }
+    }
+    resetGroup()
+    return positionedEvents
+}
+
 @Composable
 fun Schedule(
     events: List<Event>,
@@ -393,12 +471,12 @@ fun BasicSchedule(
 ) {
     val numDays = ChronoUnit.DAYS.between(minDate, maxDate).toInt() + 1
     val dividerColor = if (MaterialTheme.colors.isLight) Color.LightGray else Color.DarkGray
-    val splitEvents = remember(events) { splitEvents(events.sortedBy(Event::start)) }
+    val positionedEvents = remember(events) { arrangeEvents(splitEvents(events.sortedBy(Event::start))) }
     Layout(
         content = {
-              splitEvents.forEach { splitEvent ->
-                  Box(modifier = Modifier.eventData(splitEvent)) {
-                      eventContent(splitEvent)
+              positionedEvents.forEach { positionedEvent ->
+                  Box(modifier = Modifier.eventData(positionedEvent)) {
+                      eventContent(positionedEvent)
                   }
               }
         },
@@ -428,7 +506,8 @@ fun BasicSchedule(
             val splitEvent = measurable.parentData as PositionedEvent
             val eventDurationMinutes = ChronoUnit.MINUTES.between(splitEvent.start, splitEvent.end)
             val eventHeight = ((eventDurationMinutes / 60f) * hourHeight.toPx()).roundToInt()
-            val placeable = measurable.measure(constraints.copy(minWidth = dayWidth.roundToPx(), maxWidth = dayWidth.roundToPx(), minHeight = eventHeight, maxHeight = eventHeight))
+            val eventWidth = ((splitEvent.colSpan.toFloat() / splitEvent.colTotal.toFloat()) * dayWidth.toPx()).roundToInt()
+            val placeable = measurable.measure(constraints.copy(minWidth = eventWidth, maxWidth = eventWidth, minHeight = eventHeight, maxHeight = eventHeight))
             Pair(placeable, splitEvent)
         }
         layout(width, height) {
@@ -436,7 +515,7 @@ fun BasicSchedule(
                 val eventOffsetMinutes = ChronoUnit.MINUTES.between(LocalTime.MIN, splitEvent.start)
                 val eventY = ((eventOffsetMinutes / 60f) * hourHeight.toPx()).roundToInt()
                 val eventOffsetDays = ChronoUnit.DAYS.between(minDate, splitEvent.date).toInt()
-                val eventX = eventOffsetDays * dayWidth.roundToPx()
+                val eventX = eventOffsetDays * dayWidth.roundToPx() + (splitEvent.col * (dayWidth.toPx() / splitEvent.colTotal.toFloat())).roundToInt()
                 placeable.place(eventX, eventY)
             }
         }
